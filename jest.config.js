84e64c568e3cc8c4376a796e04e92c598345fb0e
Manu Phatak <manu@thinkcerca.com>
2018-09-27 19:48:03 -0500

    add typescript & remove babel

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -124,7 +126,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: "node"
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -159,9 +161,9 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    "^.+\\.jsx?$": "babel-jest"
-  }
+  // transform: {
+  //   "^.+\\.jsx?$": "babel-jest"
+  // }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
