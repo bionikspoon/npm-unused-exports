c4b5a1244cd79f6e2ccb63f7acc656cf339a479d
Manu Phatak <manu@thinkcerca.com>
2018-10-13 14:21:51 -0500

    map packages and non js files

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,23 +2,44 @@ import analyzeExports, { findFiles, parseFile } from '.'
 
 describe('analyzeExports', () => {
   test('it finds export usage', async () => {
-    const results = await analyzeExports('./src/__mockDirectory__', {
-      modules: ['client', 'node_modules'],
-    })
+    const results = await analyzeExports(
+      './src/__mockDirectory__',
+      './client/**/*',
+      {
+        modules: ['client'],
+      }
+    )
 
     expect(results).toEqual({
+      '@bionikspoon/package': {},
       'client/components/A.js': { default: ['client/components/App/App.js'] },
       'client/components/App/App.js': {
         default: ['client/components/App/index.js'],
       },
       'client/components/App/index.js': { default: ['client/index.js'] },
+      'client/components/App/query.graphql': {
+        default: ['client/components/App/App.js'],
+      },
+      'client/components/App/styles.css': {
+        default: [],
+      },
       'client/components/B.js': { default: ['client/components/App/App.js'] },
       'client/components/C.js': { default: ['client/components/App/App.js'] },
+      'client/constants.js': {
+        PUBLISH: ['client/components/App/App.js'],
+        SUBSCRIBE: ['client/components/App/App.js'],
+      },
       'client/index.js': {},
       'client/utils/camelcase.js': { default: [] },
       'client/utils/display.js': {
         Display: ['client/components/C.js'],
         default: ['client/components/A.js', 'client/components/B.js'],
+      },
+      react: {
+        default: ['client/index.js', 'client/components/App/App.js'],
+      },
+      'react-dom': {
+        render: ['client/index.js'],
       },
     })
   })
@@ -26,14 +47,17 @@ describe('analyzeExports', () => {
 
 describe('findFiles', () => {
   test('it finds js files in a directory', async () => {
-    const files = await findFiles('./src/__mockDirectory__')
+    const files = await findFiles('./src/__mockDirectory__', './client/**/*')
 
     expect(files).toEqual([
       './client/components/A.js',
       './client/components/App/App.js',
       './client/components/App/index.js',
+      './client/components/App/query.graphql',
+      './client/components/App/styles.css',
       './client/components/B.js',
       './client/components/C.js',
+      './client/constants.js',
       './client/index.js',
       './client/utils/camelcase.js',
       './client/utils/display.js',
@@ -63,6 +87,18 @@ describe('parseFile', () => {
       exports: ['default'],
       imports: [['default', '../utils/display']],
       pathname: './client/components/A.js',
+    })
+  })
+
+  test('parses a non js file', async () => {
+    const results = await parseFile(
+      './src/__mockDirectory__',
+      './client/components/App/query.graphql'
+    )
+    expect(results).toEqual({
+      exports: ['default'],
+      imports: [],
+      pathname: './client/components/App/query.graphql',
     })
   })
 })
